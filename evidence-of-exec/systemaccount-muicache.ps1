3254f54dddb7dd61c43111c8c7967a99216d5a93
HatsOffSec <hatsoffsec@outlook.com>
2020-08-02 19:38:23 +0100

    initial upload

---
+++ evidence-of-exec/systemaccount-muicache.ps1
@@ -0,0 +1,9 @@
+﻿# Powershell
+#
+# @Russ_Taylor_
+#
+# This script is looking at .default which shows applications running at the 
+# system level. typically this should only be anti-virus or other similar
+# software
+
+Get-Item -path  Registry::"HKEY_USERS\.DEFAULT\Software\Microsoft\Windows\ShellNoRoam\MuiCache"
