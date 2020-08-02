bd2ffd923d73d57e4fc60c295298fc55ecd045d3
HatsOffSec <hatsoffsec@outlook.com>
2020-08-02 19:31:32 +0100

    initial upload

---
+++ evidence-of-exec/appcompat-hexdump.ps1
@@ -0,0 +1,5 @@
+﻿#Powershell
+#@Russ_Taylor_
+
+Get-ItemPropertyValue -path  Registry::"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\AppCompatCache" -Name AppCompatCache |
+    Format-Hex 
