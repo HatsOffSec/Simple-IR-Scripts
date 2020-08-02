b852f91eff8e002557c63cf24e842dd8dc361085
HatsOffSec <hatsoffsec@outlook.com>
2020-08-02 19:07:30 +0100

    updated script to check for default value

--- evidence-of-exec/Check-Prefetch.ps1
+++ evidence-of-exec/Check-Prefetch.ps1
@@ -1,2 +1,6 @@
-﻿Get-Item -Path Registry::"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management\PrefetchParameters"
-    Select-Object -ExpandProperty property
+﻿$reg = Get-ItemPropertyValue -path  Registry::"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management\PrefetchParameters" -Name EnablePrefetcher
+
+if ($reg -match '3') {'Prefetch is enabled'}
+elseif ($reg -ne '3') {'non-default value found:'} 
+
+echo "The key value is set to $reg"
