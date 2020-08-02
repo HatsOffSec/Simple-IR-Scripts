b1608d3985dfea9d71fb8185f0c551358d2f8e55
HatsOffSec <hatsoffsec@outlook.com>
2020-08-02 19:14:49 +0100

    updated messages

--- evidence-of-exec/Check-Prefetch.ps1
+++ evidence-of-exec/Check-Prefetch.ps1
@@ -1,6 +1,13 @@
-﻿$reg = Get-ItemPropertyValue -path  Registry::"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management\PrefetchParameters" -Name EnablePrefetcher
+﻿#Powershell
+#@Russ_Taylor_
+#
+#credit to Adam Harrison (blog.1234n6.com) for value descriptions
 
-if ($reg -match '3') {'Prefetch is enabled'}
-elseif ($reg -ne '3') {'non-default value found:'} 
+$reg = Get-ItemPropertyValue -path  Registry::"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management\PrefetchParameters" -Name EnablePrefetcher
 
-echo "The key value is set to $reg"
+if ($reg -match '3') {'Value = 3 Both Application launch and Boot prefetching enabled'}
+elseif ($reg -match '2') {'Value = 2 Only Boot prefetching enabled'} 
+elseif ($reg -match '1') {'Value = 1 Only Application launch prefetching enabled'}
+elseif ($reg -match '0') {'Value = 0 Prefetch disabled'}
+else {"The vlaue appears to be outside of the expected range"}
+
